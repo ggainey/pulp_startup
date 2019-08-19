0967e82594e6cf17736b9843bfc8b45972f807ed
Grant Gainey <ggainey@redhat.com>
2019-08-19 16:21:11 -0400

    Fixes to centos-setup

--- devsetup/centos_vagrant_init.sh
+++ devsetup/centos_vagrant_init.sh
@@ -12,11 +12,7 @@ HERE
 # pause pulp
 pstop
 # install needed modules
-sudo yum erase -y python2-nectar
-sudo yum install -y https://repos.fedorapeople.org/repos/pulp/pulp/testing/automation/2-master/stage/7/x86_64/python-nectar-1.6.0-1.el7.noarch.rpm
-sudo yum install -y wget
-sudo yum install -y openssl-devel
-#sudo pip install pydevd-pycharm rhsm
+sudo yum install -y wget openssl-dev python2-pip
 # set download_interval to Very Fast
 #sudo ex /etc/pulp/server.conf <<HERE2
 #:%s/# download_interval: 30/download_interval: 1/
@@ -24,7 +20,7 @@ sudo yum install -y openssl-devel
 #HERE2
 cd
 cd devel/pulp
-#sudo pip install -r test_requirements.txt
+sudo pip install -r test_requirements.txt
 
 # start up pulp again
 prestart
