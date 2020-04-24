681608bef8106d32f11cfd3fcf0e6d2e433a7642
Grant Gainey <ggainey@redhat.com>
2020-04-24 13:04:50 -0400

    Fix broken exporter-name

---
+++ import_export/export_version_setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Poll a Pulp task until it is finished.
+wait_until_task_finished() {
+    echo "Polling the task until it has reached a final state."
+    local task_url=$1
+    while true
+    do
+        local response=$(http $task_url)
+        local state=$(jq -r .state <<< ${response})
+        jq . <<< "${response}"
+        case ${state} in
+            failed|canceled)
+                echo "Task in final state: ${state}"
+                exit 1
+                ;;
+            completed)
+                echo "$task_url complete."
+                break
+                ;;
+            *)
+                echo "Still waiting..."
+                sleep 1
+                ;;
+        esac
+    done
+}
+
+BASE_ADDR="admin:password@localhost:24817"
+EXPORTER_URL="/pulp/api/v3/exporters/core/pulp/"
+
+# get a FILE repo UUID
+FILE_HREF=$(http GET http://localhost:24817/pulp/api/v3/repositories/file/file/ | jq -r ".results[0] | .pulp_href")
+# get an RPM repo UUID
+RPM_HREF=$(http GET http://localhost:24817/pulp/api/v3/repositories/rpm/rpm/ | jq -r ".results[0] | .pulp_href")
+
+# create exporter
+EXPORTER_NAME="test"
+EXPORTER_HREF=$(http POST $BASE_ADDR$EXPORTER_URL name="${EXPORT_NAME}"-exporter repositories:=[\"${FILE_HREF}\",\"${RPM_HREF}\"] path=/tmp/exports/) #"
+if [ -z "$EXPORTER_HREF" ]; then exit; fi
+
+# LIST all exporters
+http GET $BASE_ADDR$EXPORTER_URL
