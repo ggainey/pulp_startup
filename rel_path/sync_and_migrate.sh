42243571270c3de25ea7611d344c7c8f8658d6d7
Grant Gainey <ggainey@redhat.com>
2021-07-19 15:07:57 -0400

    Some tweaks to sync_and_migrate

--- rel_path/sync_and_migrate.sh
+++ rel_path/sync_and_migrate.sh
@@ -2,29 +2,35 @@
 PROBLEM_NAMES=(\
 grafana \
 centos-ops \
+mysql \
 sles12-backport-sp5-standard \
 sles12-backport-sp5-product \
 sles12-backport-sp4-standard \
 sles12-backport-sp4-product \
+sles12-update-sp5 \
+sles12-debug-update-sp5 \
 )
 PROBLEM_URLS=(\
 https://packages.grafana.com/oss/rpm \
 http://ftp.cs.stanford.edu/centos/7/opstools/x86_64/ \
+http://repo.mysql.com/yum/mysql-tools-community/el/7/x86_64/ \
 https://updates.suse.com/SUSE/Backports/SLE-12-SP5_x86_64/standard/ \
 https://updates.suse.com/SUSE/Backports/SLE-12-SP5_x86_64/product/ \
 https://updates.suse.com/SUSE/Backports/SLE-12-SP4_x86_64/standard/ \
 https://updates.suse.com/SUSE/Backports/SLE-12-SP4_x86_64/product/ \
+https://updates.suse.com/SUSE/Updates/SLE-SERVER/12-SP5/x86_64/update/ \
+https://updates.suse.com/SUSE/Updates/SLE-SERVER/12-SP5/x86_64/update_debug/ \
 )
-# ordered list of repo-auth-tokens matching the URLs above. Fill from your own
-# account-access. For non-SUSE-repos, use "NULL"
+# SUSE_TOKENS is assumed to be a string which is an ordered list of repo-auth-tokens matching the 
+# URLs above. Fill from your own account-access. For non-SUSE-repos, use "NULL"
 SUSE_TOKENS=($SUSE_TOKEN_STR)
-echo AUTH : ${SUSE_AUTH}
 echo TOKENS : ${SUSE_TOKENS[@]}
+export PULP2_AUTH=`python -c 'import base64; print(base64.encodestring("admin:admin".encode())[:-1].decode())'`
 
-cleanup=
-p2setup=
+cleanup=""
+p2setup=""
 migrate="yes"
-resync="yes"
+resync=""
 
 pulp-admin login -u admin -p admin
 
@@ -33,8 +39,13 @@ if [ -n "$cleanup" ]; then
     echo ">>> REPO/REMOTE CLEANUP"
     for i in ${!PROBLEM_NAMES[@]}
     do
+        if [ $i -lt 0 ]
+        then
+            continue
+        fi
         echo clean ${PROBLEM_NAMES[$i]}
-        pulp rpm remote destroy --name ${PROBLEM_NAMES[$i]}
+        REMOTE_HREF=$(pulp rpm repository show --name ${PROBLEM_NAMES[$i]} | jq -r .remote)
+        pulp rpm remote destroy --href ${REMOTE_HREF}
         pulp rpm repository destroy --name ${PROBLEM_NAMES[$i]}
         pulp-admin rpm repo delete --repo-id ${PROBLEM_NAMES[$i]}
     done
@@ -48,16 +59,22 @@ if [ -n "$p2setup" ]; then
     echo ">>> SYNC"
     for i in ${!PROBLEM_NAMES[@]}
     do
+        if [ $i -lt 0 ]
+        then
+            continue
+        fi
         echo ${PROBLEM_NAMES[$i]} : ${PROBLEM_URLS[$i]}
         echo Token : ${SUSE_TOKENS[$i]}
+        pulp-admin rpm repo create --repo-id ${PROBLEM_NAMES[$i]} \
+            --feed ${PROBLEM_URLS[$i]} --download-policy on_demand
         if [[ "NULL" != ${SUSE_TOKENS[$i]} ]]
         then
-            URL="${PROBLEM_URLS[$i]}?${SUSE_TOKENS[$i]}"
-        else
-            URL=${PROBLEM_URLS[$i]}
+            CONFIG="{\"query_auth_token\": \"${SUSE_TOKENS[$i]}\" }"
+            echo Config : ${CONFIG}
+            http --verify no --auth admin:admin \
+                PUT https://localhost/pulp/api/v2/repositories/${PROBLEM_NAMES[$i]}/ \
+                importer_config:="${CONFIG}"
         fi
-        pulp-admin rpm repo create --repo-id ${PROBLEM_NAMES[$i]} \
-            --feed ${URL} --download-policy on_demand
         pulp-admin rpm repo sync run --repo-id ${PROBLEM_NAMES[$i]}
         TASKS_OUTPUT=""
         echo -n "Waiting on sync "
@@ -65,7 +82,7 @@ if [ -n "$p2setup" ]; then
             TASKS_OUTPUT=$(pulp-admin tasks list | grep "No tasks found")
             echo -n "."; sleep 5
         done
-        echo
+        echo -e "\n\n"
     done
 fi
 
@@ -74,8 +91,10 @@ if [ -n "$migrate" ]; then
     echo "MIGRATING"
     PLAN_HREF=$(pulp migration plan create --plan '{"plugins": [{"type": "rpm"}]}' | jq -r .pulp_href)
     echo "Plan: $PLAN_HREF"
-    pulp -T 0 migration plan run --href ${PLAN_HREF}
-    sleep 5
+    pulp migration plan run --href ${PLAN_HREF}
+    # The plan's child-tasks can take some time to complete after the plan declares itself done...
+    echo WAITING...
+    sleep 180
 fi
 
 # syncing SUSE repos post-migration fails due to https://pulp.plan.io/issues/9088
@@ -84,11 +103,16 @@ if [ -n "$resync" ]; then
     echo ">>> SYNC AUTOPUBLISH"
     for i in ${!PROBLEM_NAMES[@]}
     do
+        if [ $i -lt 0 ]
+        then
+            continue
+        fi
         echo ${PROBLEM_NAMES[$i]} : ${PROBLEM_URLS[$i]}
         echo Token : ${SUSE_TOKENS[$i]}
         REMOTE_HREF=$(pulp rpm repository show --name ${PROBLEM_NAMES[$i]} | jq -r .remote)
         echo "Remote-href: ${REMOTE_HREF}"
         pulp rpm repository sync --name ${PROBLEM_NAMES[$i]}
+        echo -e "\n\n"
     done
 fi
 
