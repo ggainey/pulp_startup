d318c5c93809c79eb82d0debd8e140731d8846c0
Grant Gainey <ggainey@redhat.com>
2021-08-12 16:10:01 -0400

    Depsolving experiments

---
+++ 1965942_depsolv/repro.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#https://cdn.redhat.com/content/dist/rhel/server/7/7Server/x86_64/os
+#https://cdn.redhat.com/content/dist/rhel8/8/x86_64/baseos/os/ 
+REPOS=(\
+https://cdn.redhat.com/content/dist/rhel/server/7/7Server/x86_64/os \
+)
+NAMES=(\
+rhel7 \
+)
+
+NUM_dups=2
+NUM_CYCLES=6
+echo "CLEANUP"
+for n in ${!NAMES[@]}
+do
+    for i in {1..1}
+    do
+        pulp rpm remote destroy --name ${NAMES[$n]}-${i}
+        pulp rpm repository destroy --name ${NAMES[$n]}-${i}
+    done
+done
+pulp orphans delete
+
+
+echo "CYCLE-START"
+for c in {1..1}
+do
+    echo "SETUP REPOS AND REMOTES"
+    for n in ${!NAMES[@]}
+    do
+        for i in {1..1}
+        do
+            pulp rpm remote create --name ${NAMES[$n]}-${i} --url ${REPOS[$n]} --policy on_demand \
+            --ca-cert "${CDN_CA_CERT}" --client-key "${CDN_CLIENT_KEY}" --client-cert "${CDN_CLIENT_CERT}" | jq .pulp_href
+            pulp rpm repository create --name ${NAMES[$n]}-${i} --remote ${NAMES[$n]}-${i} --autopublish | jq .pulp_href
+        done
+    done
+    starting_failed=`pulp task list --state failed | jq length`
+    echo "SYNCING..."
+    for n in ${!NAMES[@]}
+    do
+        for i in {1..1}
+        do
+            pulp -b rpm repository sync --name ${NAMES[$n]}-${i}
+        done
+    done
+    sleep 5
+    echo "WAIT FOR COMPLETION...."
+    while true
+    do
+        running=`pulp task list --state running | jq length`
+        echo -n "."
+        sleep 5
+        if [ ${running} -eq 0 ]
+        then
+            echo "DONE"
+            break
+        fi
+    done
+    failed=`pulp task list --state failed | jq length`
+    echo "FAILURES : ${failed}"
+    if [ ${failed} -gt ${starting_failed} ]
+    then
+      echo "FAILED: " ${failed} - ${starting_failed}
+      exit
+    fi
+    exit
+    echo "CLEANUP FOR NEXT"
+    for n in ${!NAMES[@]}
+    do
+        for i in {1..1}
+        do
+            pulp rpm remote destroy --name ${NAMES[$n]}-${i}
+            pulp rpm repository destroy --name ${NAMES[$n]}-${i}
+        done
+    done
+    pulp orphans delete
+done
+
