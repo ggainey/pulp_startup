4dc72970b182b077aae5e82fcdb566ad155911f4
Grant Gainey <ggainey@redhat.com>
2021-08-23 15:53:04 -0400

    repro can now split into many cfgs, edit them, and execute

--- 1965942_depsolv/repro.sh
+++ 1965942_depsolv/repro.sh
@@ -4,9 +4,9 @@ REPO="https://cdn.redhat.com/content/dist/rhel/server/7/7Server/x86_64/os"
 NAME="rhel7-1"
 
 sync=
-repeat=3
-create_dest=
-create_cfgs=
+repeat=7
+create_dest="yes"
+create_cfgs="yes"
 issue_copy="yes"
 
 # Sync RHEL7
@@ -37,44 +37,59 @@ echo ">>> DEST_REPO ${DEST_REPO}"
 
 
 # Create the configs
+MAX_UNITS_PER=5000
 if [ -n "$create_cfgs" ]; then
     echo ">>> CREATING CONFIGS"
+    echo ">>> ...BASE RPMS"
     echo """[
     {
     \"source_repo_version\": "${SRC_VERSION}",
     \"dest_repo\": "${DEST_REPO}",
-    \"content\": [""" > cfg_1
+    \"content\": [""" > tmp_cfg
     # config-1: just base packages/groups/envs/lang
     psql -U pulp -d pulp --host 127.0.0.1 -P tuples_only \
-        -c "select '\"/pulp/api/v3/content/rpm/packages/' || p.content_ptr_id || '/\",' from rpm_package p inner join core_repositorycontent rc on rc.content_id = p.content_ptr_id inner join core_repository r on r.pulp_id = rc.repository_id where r.name = '${NAME}' and not exists (select 1 from rpm_updatecollectionpackage ucp where ucp.name = p.name and ucp.epoch = p.epoch and ucp.version = p.version and ucp.release = p.release and ucp.arch = p.arch)" >> cfg_1
-    echo """        ]
-    }
-    ]""" >> cfg_1
+        -c "select '\"/pulp/api/v3/content/rpm/packages/' || p.content_ptr_id || '/\",' from rpm_package p inner join core_repositorycontent rc on rc.content_id = p.content_ptr_id inner join core_repository r on r.pulp_id = rc.repository_id where r.name = '${NAME}' and not exists (select 1 from rpm_updatecollectionpackage ucp where ucp.name = p.name and ucp.epoch = p.epoch and ucp.version = p.version and ucp.release = p.release and ucp.arch = p.arch)" >> tmp_cfg
+    echo """] } ]""" >> tmp_cfg
+    cat tmp_cfg | tr -s '\n' '\r' | sed -e 's/,\r]/]/g' | tr '\r' '\n' > cfg_1
 
-    # config-2: ALL packages (ie there will be dups)
-    echo """[
-    {
-    \"source_repo_version\": "${SRC_VERSION}",
-    \"dest_repo\": "${DEST_REPO}",
-    \"content\": [""" > cfg_2
-    psql -U pulp -d pulp --host 127.0.0.1 -P tuples_only \
-        -c "select '\"/pulp/api/v3/content/rpm/packages/' || p.content_ptr_id || '/\",' from rpm_package p inner join core_repositorycontent rc on rc.content_id = p.content_ptr_id inner join core_repository r on r.pulp_id = rc.repository_id where r.name = '${NAME}'" >> cfg_2
-    echo """        ]
-    }
-    ]""" >> cfg_2
+    # find ALL the packages
+    # Find out how many "all" is, and then create one cfg per-MAX-UNITS possible
+    echo ">>> ...FINDING ALL-RPM COUNT..."
+    UNITS_TO_COPY=$(psql -U pulp -d pulp --host 127.0.0.1 -P tuples_only -c "select count(p.content_ptr_id) from rpm_package p inner join core_repositorycontent rc on rc.content_id = p.content_ptr_id inner join core_repository r on r.pulp_id = rc.repository_id where r.name = '${NAME}'") 
+    echo ">>> PREPARING CFGS TO COPY ${UNITS_TO_COPY} UNITS..."
+    OFFSET=0
+    PAGE=2 # First file we'll write is cfg_2
+    while [ ${OFFSET} -lt ${UNITS_TO_COPY} ]
+    do
+        echo "PAGE/OFFSET: $PAGE/$OFFSET"
+        echo """[
+        {
+        \"source_repo_version\": "${SRC_VERSION}",
+        \"dest_repo\": "${DEST_REPO}",
+        \"content\": [""" > tmp_cfg
+        psql -U pulp -d pulp --host 127.0.0.1 -P tuples_only -c "select '\"/pulp/api/v3/content/rpm/packages/' || p.content_ptr_id || '/\",' from rpm_package p inner join core_repositorycontent rc on rc.content_id = p.content_ptr_id inner join core_repository r on r.pulp_id = rc.repository_id where r.name = '${NAME}' LIMIT ${MAX_UNITS_PER} OFFSET ${OFFSET}" >> tmp_cfg
+        echo """] } ]""" >> tmp_cfg
+        # Punt extraneous comma and newline at resulting from the query/>> above
+        echo ">>> EDIT cfg_${PAGE}..."
+        cat tmp_cfg | tr -s '\n' '\r' | sed -e 's/,\r]/]/g' | tr '\r' '\n' > cfg_${PAGE}
+        OFFSET=$(($OFFSET+$MAX_UNITS_PER))
+        PAGE=$(($PAGE+1))
+    done
 fi
 
 
 # Actually Do the Deed
 if [ -n "$issue_copy" ]; then
-    echo ">>> ISSUING COPY COMMANDS"
+    echo ">>> ISSUING COPY COMMANDS FOR ALL cfg_* FOUND"
     # Issue first copy
     TASK=$(http POST :/pulp/api/v3/rpm/copy/ dependency_solving=True config:=@./cfg_1 | jq -r .task)
     echo ">>> FIRST TASK ${TASK}"
     pulp task show --href ${TASK} --wait
 
-    # Issue second copy
-    TASK=$(http POST :/pulp/api/v3/rpm/copy/ dependency_solving=True config:=@./cfg_2 | jq -r .task)
-    echo ">>> SECOND TASK ${TASK}"
-    pulp task show --href ${TASK} --wait
+    for f in $(ls cfg_?)
+    do
+        # Issue copy-cmd
+        TASK=$(http POST :/pulp/api/v3/rpm/copy/ dependency_solving=True config:=@./cfg_2 | jq -r .task)
+        echo ">>> $f TASK ${TASK}"
+    done
 fi
