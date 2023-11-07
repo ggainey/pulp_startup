69cf2ea9a4473a7e1725a30a415dd99484fc40b5
Grant Gainey <ggainey@redhat.com>
2023-11-07 11:26:26 -0500

    Minor tweaks to rpm-domain-demo scripts

--- rpm_domain_demo/teardown.sh
+++ rpm_domain_demo/teardown.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 # Delete created repos, users, and domains
-echo ">>> Destroy ${PRIVATE_DOMAIN}-domain objects..."
+echo ">>> Destroy default-domain Repository..."
 pulp rpm repository destroy --name ${DEFAULT_REPO}
+echo ">>> Destroy ${PRIVATE_DOMAIN}-domain Remote..."
 pulp rpm remote destroy --name ${DEFAULT_REPO}
-echo ">>> Destroy ${PRIVATE_DOMAIN}-domain objects..."
+echo ">>> Destroy ${PRIVATE_DOMAIN}-domain Distribution..."
 pulp --domain ${PRIVATE_DOMAIN} rpm distribution destroy --name ${PRIVATE_REPO}
+echo ">>> Destroy ${PRIVATE_DOMAIN}-domain Repository..."
 pulp --domain ${PRIVATE_DOMAIN} rpm repository destroy --name ${PRIVATE_REPO}
 echo ">>> Destroy ${PRIVATE_DOMAIN}..."
 pulp domain destroy --name ${PRIVATE_DOMAIN}
